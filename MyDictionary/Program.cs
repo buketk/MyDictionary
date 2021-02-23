c59d4019517da8058b384b3943ac564e47641927
buketk <61161607+buketk@users.noreply.github.com>
2021-02-23 22:50:10 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(456, "Motor");
+            myDictionary.Add(888, "Scoter");
+            myDictionary.Add(999, "Bicycle");
+
+            myDictionary.ToList();
+
+
+        }
+    }
+}
